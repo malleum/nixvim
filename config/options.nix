9e7e70135768cdf3e401e454bc43df3f3b52e9b5
Joshua Hammer <jph33@outlook.com>
2024-02-29 16:14:24 -0500

    fix

--- config/options.nix
+++
@@ -1,9 +0,0 @@
-{
-  options = {
-    number = true; # Show line numbers
-    relativenumber = true; # Show relative line numbers
-
-    shiftwidth = 2; # Tab width should be 2
-  };
-  globals.mapleader = " ";
-}
