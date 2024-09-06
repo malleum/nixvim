4bbb55701380e7efd7475184cada8747204d8404
joshua hammer <jph33@outlook.com>
2024-09-06 15:38:28 -0400

    sml

--- config/lsp.nix
+++ config/lsp.nix
@@ -22,13 +22,14 @@
           htmx.enable = true;
           java-language-server.enable = true;
           jsonls.enable = true;
-          kotlin-language-server.enable = true;
           lua-ls.enable = true;
+          kotlin-language-server.enable = true;
           marksman.enable = true;
           nil-ls.enable = true;
           nixd.enable = true;
-          sqls.enable = true;
+          ocamllsp.enable = true;
           pyright.enable = true;
+          sqls.enable = true;
           rust-analyzer = {
             enable = true;
             installRustc = true;
