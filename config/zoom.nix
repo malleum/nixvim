45e99d203e3a5fe7f648006741fe4cc55939d66c
joshua hammer <jph33@outlook.com>
2024-11-05 13:52:36 -0500

    return of flash

--- config/zoom.nix
+++ config/zoom.nix
@@ -35,6 +35,17 @@
         };
       };
     };
+
+    flash = {
+      enable = true;
+      settings = {
+        label.rainbow.enabled = true;
+        modes = {
+          search.enabled = false;
+          char.enabled = false;
+        };
+      };
+    };
   };
 
   keymaps = [
@@ -57,6 +68,26 @@
       mode = ["n"];
       key = "<leader>pW";
       action = "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.expand('<cWORD>') })<cr>";
+    }
+    {
+      mode = ["n" "x" "o"];
+      key = "s";
+      action = "<cmd>lua require('flash').jump()<cr>";
+    }
+    {
+      mode = ["n" "x" "o"];
+      key = "S";
+      action = "<cmd>lua require('flash').treesitter()<cr>";
+    }
+    {
+      mode = ["o"];
+      key = "r";
+      action = "<cmd>lua require('flash').remote()<cr>";
+    }
+    {
+      mode = ["x" "o"];
+      key = "R";
+      action = "<cmd>lua require('flash').treesitter_search()<cr>";
     }
   ];
 }
